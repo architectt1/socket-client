649fd4a0b5dd3980e9815fe2183e35705415e1cf
Gian Franco Zabarino <architectt@gmail.com>
2018-09-12 02:53:39 -0300

    Also listen to the "exercise_started" event.

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const socket = io('http://localhost:3000');
 socket.on('connect', () => console.log('connected to server'));
 socket.on('disconnect', () => console.log('disconnected from server'));
 
-['start_countdown', 'exercise_finished'].forEach(event => {
+['start_countdown', 'exercise_started', 'exercise_finished'].forEach(event => {
   socket
     .on(event, (data) => {
-      console.log(`received event '${event}' with payload ${JSON.stringify(data)}`);
+      console.log(`${new Date()}: received event '${event}' with payload ${JSON.stringify(data)}`);
       if (event === 'start_countdown') {
         setTimeout(() => {
           socket.emit('cancel_exercise');
